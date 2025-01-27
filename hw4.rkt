52e381897003ddbdd0efc824b2b9877404f72d11
Diego Cicotoste <cicotosted@gmail.com>
2025-01-26 20:35:13 -0500

    work in progrss

---
+++ hw4.rkt
@@ -0,0 +1,42 @@
+ #lang lsl
+
+;; Problem 1
+
+;;(: duplicate-element-prop (-> Integer Integer Boolean))
+(define (duplicate-element-prop lst result)
+  (cond
+    [(= result -1) #t]
+    [else (>= (length (filter (lambda (x) (= x result)) lst)) 2)]))
+
+(define (sorted-lst lst)
+  (sort lst <)) 
+
+;; (: duplicate-element (->  Integer Integer))
+(define (duplicate-element lst)
+  (let ([sorted (sorted-lst lst)])
+    (cond
+      [(empty? sorted) -1] 
+      [(empty? (rest sorted)) -1]
+      [(= (first sorted) (second sorted)) (first sorted)] 
+      [else (duplicate-element (rest sorted))]))) 
+
+;; Problem 2
+
+(define (common-element-prop ...) ...)
+
+(define (common-element ...) ...)
+
+;; Problem 3
+
+(: pair-with-sum-prop (-> Integer Integer Boolean))
+(define (pair-with-sum-prop lst target)
+  (cond
+    [(empty? lst) #t]
+    [(= (+ (first lst) (second lst)) target) #t]
+    [else #f]))
+
+(define (pair-with-sum lst target)
+  (lambda (x)
+    (lambda (y)
+      (if (= (+ x y) target) (list (list x) (list y))) lst) lst)
+  '()
